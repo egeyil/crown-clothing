3c73e09ca82ae77990174db526adeb43ad638c49
egeyil <egeyildirimdevelopment@gmail.com>
2022-06-21 13:44:22 +0300

    add sign-in route, add firebase and user authentication

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,66 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAFRDdulXX4rSl-BD2yoOEE9EXFenfm6uU',
+  authDomain: 'crown-clothing-c134b.firebaseapp.com',
+  projectId: 'crown-clothing-c134b',
+  storageBucket: 'crown-clothing-c134b.appspot.com',
+  messagingSenderId: '663948063129',
+  appId: '1:663948063129:web:d94412cd37baf361372308',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+// You might need multiple providers, so that's why we use the "new" keyword
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  // Anytime a user interacts with GoogleAuthProvider we ask them
+  // to select their account
+  prompt: 'select_account',
+});
+
+// You won't need more than one authenticator, so we don't use the "new" keyword
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore(); // This points to our database inside our console
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid); // doc(database, collection, unique ID)
+
+  const userSnapShot = await getDoc(userDocRef);
+
+  // check if user data exists
+  // if it does, return userDocRef and don't do anything else
+  // if it doesn't, create/set the document with the data from userAuth in our collection
+
+  // If userSnapShot doesn't exist
+  if (!userSnapShot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  // If userSnapShot exists
+  return userDocRef;
+};
